80e7cd7edee6868d5a4e454b1ce555a180a6748c
Mohammed Afsal <mohammedafsaltut@gmail.com>
2024-02-18 08:47:03 +0530

    Handled user input

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
 
 export default function App() {
   return (
     <>
       <Header />
+      <UserInput />
     </>
   );
 }
